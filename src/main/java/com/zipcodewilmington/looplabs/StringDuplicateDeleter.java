bf57ae825fb3430f820f72f5250bd2f31b25ebce
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-30 10:30:20 -0400

    strings done

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -11,11 +11,69 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return null;
+        String[] newArr = new String[array.length];
+        int idx = 0;
+
+        for(int i = 0; i < newArr.length; i++){
+            int counter = 0;
+
+            for(int j = 0; j < newArr.length; j++){
+                if(array[i] == array[j]){
+                    counter++;
+                }
+            }
+
+            if(counter < maxNumberOfDuplications){
+                newArr[idx] = array[i];
+                idx++;
+            }
+        }
+
+        int nullIndexes = 0;
+        for(int i = 0; i < array.length; i++){
+            if(newArr[i] == null){
+                nullIndexes++;
+            }
+        }
+
+        String[] newArr2 = new String[array.length - nullIndexes];
+        for(int i = 0; i < newArr2.length; i++){
+            newArr2[i] = newArr[i];
+        }
+        return newArr2;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return null;
+        String[] newArr = new String[array.length];
+        int idx = 0;
+
+        for(int i = 0; i < newArr.length; i++){
+            int counter = 0;
+
+            for(int j = 0; j < newArr.length; j++){
+                if(array[i] == array[j]){
+                    counter++;
+                }
+            }
+
+            if(counter != exactNumberOfDuplications){
+                newArr[idx] = array[i];
+                idx++;
+            }
+        }
+
+        int nullIndexes = 0;
+        for(int i = 0; i < array.length; i++){
+            if(newArr[i] == null){
+                nullIndexes++;
+            }
+        }
+
+        String[] newArr2 = new String[array.length - nullIndexes];
+        for(int i = 0; i < newArr2.length; i++){
+            newArr2[i] = newArr[i];
+        }
+        return newArr2;
     }
 }
